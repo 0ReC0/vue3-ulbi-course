b34275df389e1c2cd400f358b9611a311ce2e79f
Kitanin Sergei <kitanin.ser@mail.ru>
2022-08-06 19:00:01 +0300

    Created app uploading posts from json placeholder with sorting, searching, pagination on posts. Created custom select, input, button. Created shared dialog component. Created components for show post, create post for local use.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,148 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <h1>Страница с постами</h1>
+
+    <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    ></my-input>
+
+    <div class="app__btns">
+      <my-button
+          @click="showDialog"
+      >Создать пост
+      </my-button>
+
+      <my-select
+          :options="sortOptions"
+          v-model="selectedSort"
+      ></my-select>
+    </div>
+
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      ></post-form>
+    </my-dialog>
+
+    <post-list
+        :posts="sortedAndSearchedPosts"
+        @remove="removePost"
+        v-if="!isPostsLoading"
+    ></post-list>
+
+    <div v-else>Идет загрузка...</div>
+
+    <my-pagination
+        v-model="page"
+        :totalPages="totalPages"
+    ></my-pagination>
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    PostForm,
+    PostList,
+  },
+
+  data() {
+    const sortOptions = [
+      {value: 'title', name: 'По названию'},
+      {value: 'body', name: 'По описанию'},
+    ];
+
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions,
+      searchQuery: ''
+    }
+  },
+
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = response.data;
+      } catch (e) {
+        alert('Ошибка');
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+  },
+
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      });
+    },
+    sortedAndSearchedPosts() {
+      if (!this.searchQuery) {
+        return this.sortedPosts;
+      }
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    }
+  },
+  watch: {
+    page() {
+      this.fetchPosts();
+    }
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <transition-group name="post-list">
+      <post-item
+          style="margin-top: 15px"
+          v-for="post in posts"
+          :key="post.id"
+          :post="post"
+          @remove="$emit('remove', post)"
+      ></post-item>
+    </transition-group>
+  </div>
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
+
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+
+export default {
+  name: "PostList",
+  components: {
+    PostItem
+  },
+
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  },
+};
+</script>
+
+<style scoped>
+.post-list-move,
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+</style>
